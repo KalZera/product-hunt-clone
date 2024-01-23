9949af00fc9200b09edf3332fcc93ee735700766
Alexander Karuzoo <kalex.codr@gmail.com>
2024-01-23 08:44:31 -0300

    [Arch 0.0.5] Add prettier ans linter for devs  (#4)

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,7 +2,9 @@ import { Route, Routes as RoutesWrapper } from 'react-router-dom';
 import { Home } from '../Pages/Home/HomePage';
 
 export const Routes = () => {
-  return (<RoutesWrapper>
-    <Route path="/" element={<Home />} />
-  </RoutesWrapper>);
-}
+  return (
+    <RoutesWrapper>
+      <Route path="/" element={<Home />} />
+    </RoutesWrapper>
+  );
+};
