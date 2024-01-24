dfba9908163bf5c4ed0a97ad0bb7f5747d66084b
Alexander Karuzoo <kalex.codr@gmail.com>
2024-01-24 08:40:00 -0300

    [0.0.8] first refactor

--- src/Pages/Home/HomePage.tsx
+++ src/Pages/Home/HomePage.tsx
@@ -1,8 +1,36 @@
 export function Home() {
   return (
     <div className="flex flex-col h-[100vh]">
-      <header className="h-10">
-        <div>Header Menu</div>
+      <header className="border rounded-md ">
+        <div className="flex m-2 p-2 justify-between items-center">
+          <div className="flex ">
+            <input
+              type="text"
+              placeholder="search"
+              className="input input-bordered input-sm"
+            />
+          </div>
+          <div className="flex flex-1">
+            <button className="btn btn-link" type="button">
+              Products
+            </button> 
+            <button className="btn btn-link" type="button">
+              Category
+            </button>
+            <button className="btn btn-link" type="button">
+              About
+            </button>
+          </div>
+          <div className="flex justify-around">
+            <button className="btn btn-neutral" type="button">
+              Login
+            </button>
+            <div className="w-2"/>
+            <button className="btn btn-neutral" type="button">
+              Register
+            </button>
+          </div>
+        </div>
       </header>
       <hr />
       <section className="flex-1">
