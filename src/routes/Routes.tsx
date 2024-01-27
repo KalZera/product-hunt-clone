769ef8fd043a3002da399c6e7833acc7fd4bfa2a
Alexander Karuzoo <kalex.codr@gmail.com>
2024-01-27 10:35:19 -0300

    [0.0.12] first refactor routes to embed clerk

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,57 @@
 import { Route, Routes as RoutesWrapper } from 'react-router-dom';
 import { Home } from '../Pages/Home/HomePage';
+import {
+  RedirectToSignIn,
+  SignIn,
+  SignInButton,
+  SignUp,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/clerk-react';
 
 export const Routes = () => {
   return (
     <RoutesWrapper>
-      <Route path="/" element={<Home />} />
+      <Route
+        path="/"
+        element={
+          <>
+            <SignedIn>
+              <UserButton />
+              <Home />
+            </SignedIn>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+          </>
+        }
+      />
+      <Route
+        path="/sign-in/*"
+        element={
+          <SignIn redirectUrl={'/home'} routing="path" path="/sign-in" />
+        }
+      />
+      <Route
+        path="/sign-up/*"
+        element={
+          <SignUp redirectUrl={'/home'} routing="path" path="/sign-up" />
+        }
+      />
+      <Route
+        path="/home"
+        element={
+          <>
+            <SignedIn>
+              <Home />
+            </SignedIn>
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
+          </>
+        }
+      />
     </RoutesWrapper>
   );
 };
