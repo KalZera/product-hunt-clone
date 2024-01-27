fc3f9c51490913a7a68d8ff2687f841940187dad
Alexander Karuzoo <kalex.codr@gmail.com>
2024-01-27 10:33:48 -0300

    add config clerk on project

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from './routes';
+import { ClerkProvider } from '@clerk/clerk-react';
+
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes />
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <Routes />
+      </ClerkProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
