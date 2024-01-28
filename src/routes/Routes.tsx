5a7b7a6e80e5005e6ef6c870172ab2cd6fa0732e
Alexander Karuzoo <kalex.codr@gmail.com>
2024-01-28 11:51:24 -0300

    refactor for best practices

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,57 +1,45 @@
-import { Route, Routes as RoutesWrapper } from 'react-router-dom';
-import { Home } from '../Pages/Home/HomePage';
 import {
-  RedirectToSignIn,
-  SignIn,
-  SignInButton,
-  SignUp,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/clerk-react';
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
+import { Home } from '../Pages/Home/HomePage';
+import { SignIn, SignUp } from '@clerk/clerk-react';
+import { ContentLayout } from './content-layout';
+import { RootLayout } from './root-layout';
 
 export const Routes = () => {
-  return (
-    <RoutesWrapper>
-      <Route
-        path="/"
-        element={
-          <>
-            <SignedIn>
-              <UserButton />
-              <Home />
-            </SignedIn>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-          </>
-        }
-      />
-      <Route
-        path="/sign-in/*"
-        element={
-          <SignIn redirectUrl={'/home'} routing="path" path="/sign-in" />
-        }
-      />
-      <Route
-        path="/sign-up/*"
-        element={
-          <SignUp redirectUrl={'/home'} routing="path" path="/sign-up" />
-        }
-      />
-      <Route
-        path="/home"
-        element={
-          <>
-            <SignedIn>
-              <Home />
-            </SignedIn>
-            <SignedOut>
-              <RedirectToSignIn />
-            </SignedOut>
-          </>
-        }
-      />
-    </RoutesWrapper>
-  );
+  const routes = createBrowserRouter([
+    {
+      path: '/',
+      element: <RootLayout />,
+      children: [
+        {
+          path: '/',
+          element: <ContentLayout />,
+          children: [
+            {
+              path: '/',
+              element: <Navigate to="/home" />,
+            },
+            { path: '/home', element: <Home /> },
+          ],
+        },
+        {
+          path: '/sign-in',
+          element: (
+            <SignIn redirectUrl={'/home'} routing="path" path="/sign-in" />
+          ),
+        },
+        {
+          path: '/sign-up/*',
+          element: (
+            <SignUp redirectUrl={'/home'} routing="path" path="/sign-up" />
+          ),
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={routes} />;
 };
