1abd593287b05718f289daf1ef91ef6f51448180
Alexander Karuzoo <kalex.codr@gmail.com>
2024-01-28 11:51:52 -0300

    reafactor for best practices pages

--- src/Pages/Home/HomePage.tsx
+++ src/Pages/Home/HomePage.tsx
@@ -1,7 +1,10 @@
+import { useAuth } from '@clerk/clerk-react';
 import { Content } from './Content';
 import { Header } from './Header';
 
 export function Home() {
+  const { userId } = useAuth();
+  console.log({ userId });
   return (
     <div className="flex flex-col h-[100vh]">
       <Header />
