4949f978422a8930c3e31eefc6466944db555552
Alexander Karuzoo <kalex.codr@gmail.com>
2024-01-29 08:40:02 -0300

    add some data at column 2

--- src/Pages/Home/Content.tsx
+++ src/Pages/Home/Content.tsx
@@ -11,8 +11,8 @@ export function Content() {
           <button className="btn btn-accent">Tech</button>
         </div>
       </div>
-      <div className="flex w-full columns-2 ">
-        <div className="w-[70%] bg-gray-200">
+      <div className="flex w-full columns-3 ">
+        <div className="flex-auto max-w-[70%] bg-gray-200">
           <div>
             <p className="text-xl font-bold my-2">The next successful app </p>
           </div>
@@ -39,7 +39,41 @@ export function Content() {
             ))}
           </div>
         </div>
-        <div className="w-[30%]"> column 2</div>
+        <div className="divider divider-horizontal" />
+        <div className="flex-1">
+          <div>
+            <p className="text-xl font-bold my-2">Products reviewed by us </p>
+          </div>
+          <div className="">
+            {products.map(app => (
+              <div className="flex gap-2 m-4 items-center" key={app.id}>
+                <div className="flex-1">
+                  <p className="text-xl font-bold">{app.title}</p>
+                  <p className="text-md">{app.description}</p>
+                  <p className="text-md">stars</p>
+                </div>
+                <div className="">
+                  <img
+                    src={app.url}
+                    alt={app.title}
+                    className="rounded-md w-12"
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-center">
+            <p className="text-xl font-bold my-2">soon </p>
+          </div>
+          <div>
+            <div className="flex gap-2 m-4 items-center">
+              <div className="flex-1">
+                <p className="text-2xl font-bold">{products[0].title}</p>
+                <p className="text-md">{products[0].description}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
