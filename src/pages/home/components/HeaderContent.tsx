b97b814bf87493891252733c97fa6ce4be6d1604
Alexander Karuzo <kalex.codr@gmail.com>
2024-02-04 16:12:49 -0300

    change component

--- src/pages/home/components/HeaderContent.tsx
+++ src/pages/home/components/HeaderContent.tsx
@@ -1,20 +1,15 @@
+import { Categories } from '../../../services/services/GetCategory';
+
 export function HeaderContent() {
   return (
     <div className="flex w-full items-center p-4 rounded-lg border ">
       <span className="m-3">Trending Topics</span>
       <div className="flex gap-2">
-        <button className="btn btn-sm ">
-          <p className="px-2">AI</p>
-        </button>
-        <button className="btn btn-sm ">
-          <p className="px-2">Marketing</p>
-        </button>
-        <button className="btn btn-sm ">
-          <p className="px-2">Saas</p>
-        </button>
-        <button className="btn btn-sm ">
-          <p className="px-2">Tech</p>
-        </button>
+        {Categories.map(category => (
+          <button className="btn btn-sm" key={category.id}>
+            <p className="px-2">{category.title}</p>
+          </button>
+        ))}
       </div>
     </div>
   );
