c9ca4aadbff1e083df48b7baa9341211bce4766a
Alexander Karuzoo <kalex.codr@gmail.com>
2024-02-10 22:09:54 -0300

    [0.0.18] add some responsive design

--- src/pages/home/ReviewedBox.tsx
+++ src/pages/home/ReviewedBox.tsx
@@ -4,21 +4,22 @@ export function ReviewedBox() {
   return (
     <div className="flex-1">
       <div>
-        <p className="text-xl font-bold my-2">Products reviewed by us </p>
+        <p className="text-2xl font-bold my-2">Products reviewed by us </p>
       </div>
       <div className="">
         {mockProducts.map(app => (
           <ProductReviewed app={app} key={app.id} />
         ))}
       </div>
-      <div className="flex justify-center">
-        <p className="text-xl font-bold my-2">soon </p>
+      <div className="flex">
+        <p className="text-2xl font-bold my-2">Coming Soon </p>
       </div>
       <div>
         <div className="flex gap-2 m-4 items-center">
           <div className="flex-1">
-            <p className="text-2xl font-bold">{mockProducts[0].title}</p>
+            <p className="text-xl font-bold">{mockProducts[0].title}</p>
             <p className="text-md">{mockProducts[0].description}</p>
+            <p className="text-md">+ follow</p>
           </div>
         </div>
       </div>
--- src/pages/home/components/HeaderContent.tsx
+++ src/pages/home/components/HeaderContent.tsx
@@ -4,7 +4,7 @@ export function HeaderContent() {
   return (
     <div className="flex w-full items-center p-4 rounded-lg border mb-2 ">
       <span className="m-3">Trending Topics</span>
-      <div className="flex gap-2">
+      <div className="flex gap-2 sm:overflow-hidden overflow-x-scroll">
         {Categories.map(category => (
           <button className="btn btn-sm" key={category.id}>
             <p className="px-2">{category.title}</p>
