08fc2f6f955b1429b6db98dc792bc47a0f18b686
Alexander Karuzoo <kalex.codr@gmail.com>
2024-02-13 09:36:44 -0300

    fix: mock products

--- src/services/api/Product.ts
+++ src/services/api/Product.ts
@@ -1,9 +1,12 @@
+import { Category } from './Category';
+
 export interface Product {
   id: string;
   title: string;
   description: string;
   url: string;
   upvotes: number;
+  categories: Pick<Category, 'id'>[];
   createdAt: Date;
   updatedAt: Date;
 }
