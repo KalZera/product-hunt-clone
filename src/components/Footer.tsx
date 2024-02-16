92400e00837b173e36de9fb52fc57372738809fe
Alexander Karuzoo <kalex.codr@gmail.com>
2024-02-16 16:47:55 -0300

    recreate files

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="">
+      <div className="h-[60px] flex flex-col justify-center items-center">
+        <p className="text-sm">Made by Alexander Karuzoo - GitHub @KalZera</p>
+      </div>
+    </footer>
+  );
+}
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,6 +1,5 @@
 import { Content } from './Content';
-import { Header } from './Header';
-import { Footer } from './Footer';
+import { Header, Footer } from '../../components';
 
 export function Home() {
   return (
