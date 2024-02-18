8bcd20fafd2f39b7d4fe8743b20ee0c3ba1282bb
Alexander Karuzoo <kalex.codr@gmail.com>
2024-02-18 17:10:29 -0300

    fix address logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useAuth } from '@clerk/clerk-react';
 import { useNavigate } from 'react-router-dom';
-import Logo from '../../assets/logo.svg';
+import Logo from '../assets/logo.svg';
 
 export function Header() {
   const { userId, signOut } = useAuth();
