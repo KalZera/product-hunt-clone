97fdbfefdcec73f7d27ee23c66b0254e19b80d78
Alexander Karuzoo <kalex.codr@gmail.com>
2024-02-22 22:09:11 -0300

    refactor list products

--- src/pages/home/components/ProductBox.tsx
+++ src/pages/home/components/ProductBox.tsx
@@ -1,27 +1,36 @@
+import { useMemo } from 'react';
 import { mockProducts } from '../../../services/services/GetProduct';
 import { LineProduct } from './LineProduct';
+import { EmptyList } from '../../../components';
 type Props = {
   categorySelected: string | null;
 };
 export function ProductBox({ categorySelected }: Props) {
+  const products = useMemo(() => {
+    return mockProducts.filter(app =>
+      !categorySelected ? true : app.categories.includes(categorySelected),
+    );
+  }, [categorySelected]);
   return (
     <div className="flex-auto lg:max-w-[70%]">
       <div>
         <p className="text-xl font-bold my-2">The next successful app </p>
       </div>
-      <div className="">
-        {mockProducts.map(app =>
-          !categorySelected ? (
-            <LineProduct app={app} key={app.id} />
-          ) : (
-            app.categories.includes(categorySelected) && (
+      <div
+        className={!products.length ? 'flex items-center flex-col w-full' : ''}
+      >
+        {products.length ? (
+          <>
+            {products.map(app => (
               <LineProduct app={app} key={app.id} />
-            )
-          ),
+            ))}
+            <div>
+              <button className="btn btn-primary btn-block">Show more</button>
+            </div>
+          </>
+        ) : (
+          <EmptyList />
         )}
-        <div>
-          <button className="btn btn-primary btn-block">Show more</button>
-        </div>
       </div>
     </div>
   );
