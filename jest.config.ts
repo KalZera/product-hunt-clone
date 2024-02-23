9de523af071e879fa502ce868e94fbed71b7b807
Alexander Karuzoo <kalex.codr@gmail.com>
2024-02-23 12:47:19 -0300

    fix configs to run tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,15 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
+  setupFiles: ['dotenv/config'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
     // process `*.tsx` files with `ts-jest`
+    '.+\\.(svg|css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)?$':
+      'jest-transform-stub',
   },
   moduleNameMapper: {
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
+    '\\.(gif|ttf|eot|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
+    '^.+\\.svg$': 'jest-transform-stub',
   },
 };
