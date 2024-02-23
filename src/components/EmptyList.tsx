958c35f00f051f5a61660bb7e81b5e695a81978b
Alexander Karuzoo <kalex.codr@gmail.com>
2024-02-22 22:02:39 -0300

    create empty list

---
+++ src/components/EmptyList.tsx
@@ -0,0 +1,7 @@
+export const EmptyList = () => {
+  return (
+    <div className="flex content-center items-center">
+      <span className="text-lg font-bold"> Sorry, No Data</span>
+    </div>
+  );
+};
