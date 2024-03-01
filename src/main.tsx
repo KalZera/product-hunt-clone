583789a7e982e6b1181289f3b3d60b0a27a8d9aa
Alexander Karuzoo <kalex.codr@gmail.com>
2024-03-01 16:32:58 -0300

    add config react query

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Routes } from './routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 async function enableMocking() {
   if (
@@ -18,10 +19,14 @@ async function enableMocking() {
   return worker.start();
 }
 
+const queryClient = new QueryClient();
+
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-      <Routes />
+      <QueryClientProvider client={queryClient}>
+        <Routes />
+      </QueryClientProvider>
     </React.StrictMode>,
   );
 });
