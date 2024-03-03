b4156ce539ef85ea4ac5e9c80de54d5a1f8b669b
Alexander Karuzoo <kalex.codr@gmail.com>
2024-03-02 21:28:35 -0300

    create loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export const Loading = () => {
+  return (
+    <div className="flex content-center items-center">
+      <span className="loading loading-spinner loading-md" />
+    </div>
+  );
+};
