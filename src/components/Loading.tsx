3386b646ecdf629d07fcbcb66e27befbb51d05f4
Alexander Karuzoo <kalex.codr@gmail.com>
2024-03-03 22:28:15 -0300

    add size in loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,11 @@
-export const Loading = () => {
+interface Props {
+  size: 'sm' | 'md' | 'lg' | 'xl';
+}
+
+export const Loading = ({ size = 'md' }: Props) => {
   return (
     <div className="flex content-center items-center">
-      <span className="loading loading-spinner loading-md" />
+      <span className={`loading loading-spinner loading-${size}`} />
     </div>
   );
 };
