0824bae2620c1ead007d699650dc283f31e0f73a
Alexander Karuzoo <kalex.codr@gmail.com>
2024-03-14 22:13:05 -0300

    change mb

--- src/pages/home/components/HeaderContent.tsx
+++ src/pages/home/components/HeaderContent.tsx
@@ -4,7 +4,7 @@ type PropsHeader = {
 
 export function HeaderContent({ children }: PropsHeader) {
   return (
-    <div className="flex w-full items-center p-4 rounded-lg border mb-4">
+    <div className="flex w-full items-center p-4 rounded-lg border mb-2">
       <span className="m-3">Trending Topics</span>
       <div className="flex gap-2 sm:overflow-hidden overflow-x-scroll">
         {children}
