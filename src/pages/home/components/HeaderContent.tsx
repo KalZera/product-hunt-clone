84eb98752c98da912a14ca2acfe04867670ac70b
Alexander Karuzoo <kalex.codr@gmail.com>
2024-03-15 08:14:22 -0300

    fix margin

--- src/pages/home/components/HeaderContent.tsx
+++ src/pages/home/components/HeaderContent.tsx
@@ -5,7 +5,7 @@ type PropsHeader = {
 export function HeaderContent({ children }: PropsHeader) {
   return (
     <div className="flex w-full items-center p-4 rounded-lg border mb-2">
-      <span className="m-3">Trending Topics</span>
+      <span className="m-2">Trending Topics</span>
       <div className="flex gap-2 sm:overflow-hidden overflow-x-scroll">
         {children}
       </div>
