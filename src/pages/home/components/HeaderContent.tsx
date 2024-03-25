5e96bec80d68e80101f59185940ee5b08059d5e9
Alexander Karuzoo <kalex.codr@gmail.com>
2024-03-24 23:05:02 -0300

    changins padding

--- src/pages/home/components/HeaderContent.tsx
+++ src/pages/home/components/HeaderContent.tsx
@@ -4,7 +4,7 @@ type PropsHeader = {
 
 export function HeaderContent({ children }: PropsHeader) {
   return (
-    <div className="flex w-full items-center p-2 rounded-lg border mb-2">
+    <div className="flex w-full items-center p-4 rounded-lg border mb-2">
       <span className="m-2">Trending Topics</span>
       <div className="flex gap-2 sm:overflow-hidden overflow-x-scroll">
         {children}
